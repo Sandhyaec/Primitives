bf374477cb3c22c4b54e7e4a64c140abefa65327
Sandhyaec <200701210@rajalakshmi.edu.in>
2023-04-19 08:57:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/graphicalprimitives/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.graphicalprimitives
+
+package com.example.graphicalprimitives
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
